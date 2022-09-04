4c41becd49305adf35598d41aaf3c544153fd00d
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-09-04 13:32:45 +0200

    feat(routing): :sparkles: change BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
