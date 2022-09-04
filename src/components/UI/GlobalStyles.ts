b509048fe949491c4f7eca179b3f4b2625a78c78
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-09-04 11:50:27 +0200

    feat(styles): :lipstick: define global styles
    Link and use Open Sans font.
    
    Relates to #13

---
+++ src/components/UI/GlobalStyles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  html {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 1.6rem;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  #root, #__next {
+    isolation: isolate;
+  }
+`;
