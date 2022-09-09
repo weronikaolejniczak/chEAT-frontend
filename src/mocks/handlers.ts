b93352d858e9a624435d6e3f484458353df6ff2b
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-09-09 12:19:42 +0200

    chore(test-suite): :clown_face: mock Google API auth request
    Relates to #16

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,11 @@
-export const handlers = [];
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.post(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
+    (_req, res, ctx) => {
+      // sessionStorage.setItem('is-authenticated', 'true');
+      return res(ctx.status(200));
+    }
+  ),
+];
