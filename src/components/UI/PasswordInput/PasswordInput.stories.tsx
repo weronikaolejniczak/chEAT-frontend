cb577fe9d555cfa110ebbb4132e4656a744858f1
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-09-11 15:38:23 +0200

    test(login): :white_check_mark: test PasswordInput component
    Relates to #16

--- src/components/UI/PasswordInput/PasswordInput.stories.tsx
+++ src/components/UI/PasswordInput/PasswordInput.stories.tsx
@@ -1,10 +1,11 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { expect, within, user } from 'utils/plays';
 import { StorybookWrapper } from 'components/layouts/StorybookWrapper';
 import { PasswordInput } from './PasswordInput';
 
 export default {
-  title: 'UI/Input',
+  title: 'UI/PasswordInput',
   component: PasswordInput,
   decorators: [
     (Story) => (
@@ -23,4 +24,28 @@ export const DefaultPasswordInput = Template.bind({});
 DefaultPasswordInput.args = {
   id: 'password',
   label: 'Password',
+};
+DefaultPasswordInput.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const passwordField = await canvas.findByLabelText(/password/i);
+  await user.type(passwordField, 'SomePassword', { delay: 100 });
+
+  await expect(passwordField).toHaveAttribute('type', 'password');
+};
+
+export const VisiblePasswordInput = Template.bind({});
+VisiblePasswordInput.args = {
+  id: 'password',
+  label: 'Password',
+};
+VisiblePasswordInput.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const passwordField = await canvas.findByLabelText(/password/i);
+  await user.type(passwordField, 'SomePassword', { delay: 100 });
+  const showPasswordIcon = await canvas.findByTestId('show-password');
+  await user.click(showPasswordIcon);
+
+  await expect(passwordField).toHaveAttribute('type', 'text');
 };
