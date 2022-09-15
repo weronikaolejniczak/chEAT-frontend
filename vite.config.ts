69b8fd45c390cd4afabc0b34a10230aaa5c21c0c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-09-15 20:36:40 +0200

    chore(pwa): :wrench: configure PWA plugin
    Closes #29

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    vitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'chEAT - Your personal coach',
+        short_name: 'chEAT',
+        icons: [
+          {
+            src: '/assets/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#0280a6',
+        background_color: '#fcfcfc',
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
 });
